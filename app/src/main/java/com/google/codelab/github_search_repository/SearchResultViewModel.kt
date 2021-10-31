0ddf9c023417d0873825081ded027fa3e847ca0a
ike04 <boc.0409@outlook.jp>
2021-10-31 14:21:14 +0900

    Disposableの追加

--- app/src/main/java/com/google/codelab/github_search_repository/SearchResultViewModel.kt
+++ app/src/main/java/com/google/codelab/github_search_repository/SearchResultViewModel.kt
@@ -3,6 +3,8 @@ package com.google.codelab.github_search_repository
 import androidx.lifecycle.ViewModel
 import com.google.codelab.github_search_repository.model.SearchRepositoryBusinessModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.subjects.PublishSubject
 import javax.inject.Inject
@@ -12,9 +14,16 @@ class SearchResultViewModel @Inject constructor(
     private val repository: SearchDataRepository
 ) : ViewModel() {
     val repositoryList: PublishSubject<List<SearchRepositoryBusinessModel>> = PublishSubject.create()
+    private val disposable = CompositeDisposable()
 
     fun fetchRepository() {
         repository.fetchRepository()
             .subscribeBy { repositoryList.onNext(it) }
+            .addTo(disposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
