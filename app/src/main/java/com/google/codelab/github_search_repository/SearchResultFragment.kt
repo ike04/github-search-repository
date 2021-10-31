0e239e9e5ea83967c68181fb3304e48c91032482
ike04 <boc.0409@outlook.jp>
2021-10-31 13:37:23 +0900

    viewModelの仮実装

--- app/src/main/java/com/google/codelab/github_search_repository/SearchResultFragment.kt
+++ app/src/main/java/com/google/codelab/github_search_repository/SearchResultFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.google.codelab.github_search_repository.databinding.FragmentSearchResultBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchResultFragment : Fragment() {
     private lateinit var binding: FragmentSearchResultBinding
+    private val viewModel: SearchResultViewModel by viewModels()
 
     companion object {
         fun newInstance(): SearchResultFragment {
---
+++ app/src/main/java/com/google/codelab/github_search_repository/SearchResultViewModel.kt
@@ -0,0 +1,8 @@
+package com.google.codelab.github_search_repository
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SearchResultViewModel constructor(): ViewModel() {
+}
