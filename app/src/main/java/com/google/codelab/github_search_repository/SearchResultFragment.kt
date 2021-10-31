cfddce6546dbce1c3aab14035adff99faff97ccc
ike04 <boc.0409@outlook.jp>
2021-10-31 13:07:20 +0900

    SearchResultFragmentの追加

---
+++ app/src/main/java/com/google/codelab/github_search_repository/SearchResultFragment.kt
@@ -0,0 +1,20 @@
+package com.google.codelab.github_search_repository
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.codelab.github_search_repository.databinding.FragmentSearchResultBinding
+
+class SearchResultFragment : Fragment() {
+    private lateinit var binding: FragmentSearchResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchResultBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
